cc94796bc7a6572525f7623d3b9b527cde84cd4c
Divinity-dev <divine_asiriuwa@yahoo.com>
2026-04-24 10:11:11 +0100

    fix server issue

--- server.js
+++ server.js
@@ -25,16 +25,13 @@ app.use("/api/post", postRoute);
 app.use("/api/category", categoryRoute);
 app.use("/api/subscribers", subscribersRoute);
 
-// Start server before bd connect DB connects
+// Start server AFTER DB connects
 const startServer = async () => {
+  await connectDB();
+
   app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
-  });
-
-  connectDB().catch(err => {
-    console.error("DB connection failed:", err);
   });
 };
 
 startServer();
-
