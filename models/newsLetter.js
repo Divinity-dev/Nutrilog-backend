c917a4eec035c5e2059389b0ffbe817efb8fef55
Divinity-dev <divine_asiriuwa@yahoo.com>
2026-05-03 16:02:18 +0100

    fix newsletter

--- models/newsLetter.js
+++ models/newsLetter.js
@@ -7,12 +7,9 @@ export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4, 
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
-  requireTLS: true,
-  tls: {
-    minVersion: "TLSv1.2"
-  }
 });
